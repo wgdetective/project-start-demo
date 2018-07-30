de83fec348aec47df339da54ff13dc1e6b4f95a1
Wladimir Litvinov <w.litvinov@gpsolutions.com>
2018-07-30 17:32:10 +0300

    init

---
+++ src/main/java/com/wgdetective/projectstartdemo/ProjectStartDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wgdetective.projectstartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStartDemoApplication.class, args);
+	}
+}
